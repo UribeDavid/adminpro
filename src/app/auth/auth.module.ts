788fd1db314ef09201909e0c5d2a1838dcf50081
David Uribe <daviduribe1997@gmail.com>
2020-09-12 19:55:45 -0500

    Servicios, rutas

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -12,6 +13,7 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule
   ],
   exports: [
---
+++ src/app/services/sidebar.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SidebarService {
+
+  menu: any[] = [
+    {
+      titulo: 'Dashboard',
+      icono: 'mdi mdi-gauge',
+      submenu: [
+        {
+          titulo: 'Main',
+          path: '/'
+        },
+        {
+          titulo: 'Progress Bar',
+          path: 'progress',
+        },
+        {
+          titulo: 'Gráficas',
+          path: 'grafica1'
+        }
+      ]
+    }
+  ]
+
+  constructor() { }
+}
